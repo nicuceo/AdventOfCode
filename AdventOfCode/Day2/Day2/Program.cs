ecafc45ee8c5a18cd8a63dc4fe9d2979cfa64e39
Nicolae Ceobotaru <nicuc@core-assoc.com>
2019-02-19 10:15:33 +0200

    Day2

---
+++ AdventOfCode/Day2/Day2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace Day2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var words = File.ReadAllLines("C:\\Users\\nick_\\source\\repos\\AdventOfCode\\Day2\\Day2\\input.txt");
+            var twoLettersCounter = 0;
+            var threeLettersCounter = 0;
+
+            foreach (var word in words)
+            {
+                var letters = word.ToCharArray().Distinct();
+                var twoLetterFound = false;
+                var threeLetterFound = false;
+                foreach (var letter in letters)
+                {
+                    var noOfPresence = word.Count(chr => letter.Equals(chr));
+
+                    if (noOfPresence == 2 && !twoLetterFound)
+                    {
+                        twoLettersCounter++;
+                        twoLetterFound = true;
+                    }
+                    else if (noOfPresence == 3 && !threeLetterFound)
+                    {
+                        threeLettersCounter++;
+                        threeLetterFound = true;
+                    }
+                }
+            }
+
+            var result = twoLettersCounter * threeLettersCounter;
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+    }
+}
